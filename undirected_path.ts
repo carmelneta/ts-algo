f78fe62dc903c19406d43588db5f1420dceabf50
CarmelNeta <carmelneta@gmail.com>
2024-02-25 09:42:06 +0200

    Update undirected_path.ts

--- undirected_path.ts
+++ undirected_path.ts
@@ -1,1 +1,51 @@
-const
+const undirectedPath = (edges, nodeA, nodeB): boolean => { 
+    const graph = buildGraph(edges)
+    return _hasPath(graph, nodeA, nodeB, new Set())
+}
+
+const _hasPath = (graph, src, dst, visited): boolean => {
+    if (src === dst) { 
+        return true
+    }
+    
+    if (visited.has(src)) {
+        return false
+    }
+
+    visited.add(src)
+    
+    for (let neighbor of graph[src]) {
+        if (_hasPath(graph, neighbor, dst, visited) === true) {
+            return true
+        }
+    }
+
+    return true
+}
+
+const buildGraph = (edges) => {
+    const graph = {}
+    for (let edge of edges) {
+        const [a,b] = edge
+
+        if (!(a in graph)) {
+            graph[a] = []
+        }
+
+        if (!(b in graph)) {
+            graph[b] = []
+        }
+
+        graph[a].push(b)
+        graph[b].push(a)
+    }
+    return graph
+}
+
+const edges = [
+    ['i', 'j'],
+    ['k', 'i'],
+    ['m', 'k'],
+    ['k', 'l'],
+    ['o', 'n']
+]
